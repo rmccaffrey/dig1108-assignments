930b4aef27a3df43266d4c894cabc7fe53888396
OpenShift guest <rmccaffrey@mail.valenciacollege.edu>
2012-11-10 11:48:22 -0500

    first setup of the Matrix dojo files

---
+++ Matrix_production_code.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+    *this is the production code for the Matrix dojo
+    
+*/
+
+echo "Enter the Matrix";
---
+++ Matrix_tests.php
@@ -0,0 +1,24 @@
+<?php
+/**
+    *testing module for the Matrix dojo
+
+    *Specs are:
+    
+    *Imagine a hallway with 50 closed doors on each side
+    
+    *Go to the end of the hall, back up and open every door
+    
+    *Go back and close even numbered doors
+    
+    *Go back and every third door, open if closed, close if open
+    
+    *Repeat 100 times.
+    
+    *Tell me the state of all doors
+    
+    *@
+    
+*/
+
+require_once "Matrix_production_code.php";
+
