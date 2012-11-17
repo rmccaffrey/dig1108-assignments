40a585c49c60514bfde6784595b58f63673fc697
OpenShift guest <rmccaffrey@mail.valenciacollege.edu>
2012-11-17 12:19:48 -0500

    we were in a happy place in class at this point

--- Matrix_production_code.php
+++ Matrix_production_code.php
@@ -5,8 +5,26 @@
     
 */
 
-echo "Enter the Matrix";
+echo "Enter the Matrix Production Code";
 
-$doors = array();
-$doors = array_fill(1,100,"Closed");
+// Create an array and fill it.
+$doors= array ( ); 
+$doors = array_fill (1,100,'closed');
 
+
+// All 100 doors are incrementing. If the doors are closed we're opening it.
+for ($i = 1; $i <= 100; $i++ )
+{
+    if ( $doors [ $i ] == 'closed')
+         $doors [ $i ] = 'open'; 
+}
+
+// switch state from open to close on EVEN doors
+for ($i = 1; $i <= 100; $i++ )
+{  
+   if (( $i % 2 ) == 0 )
+    {
+        if ( $doors [ $i ] == 'open')
+         $doors [ $i ] = 'closed'; 
+    } /* end if */    
+}/* end for loop */         
--- Matrix_tests.php
+++ Matrix_tests.php
@@ -8,20 +8,51 @@
  *
  * Go to the end of the hall, back up and open every door
  *
- * Go back and close even numbered doors
+ * Go back and close even-numbered doors
  *
  * Go back and every third door, open if closed, close if open
  *
- * Repeat 100 times.
+ * Continue the iteration 100 times.
  *
  * Tell me the state of all doors   
  */
 
 require_once "Matrix_production_code.php";
 
-if (assert(count($doors) == 100))
-    echo 'Passed: doors = 100';
-    
-if (assert ($doors[100] == 'Open'))
-    echo 'Passed: doors = \'Open';
+// This section is to determine if an array $doors exists.
+if  ( is_array ($doors))
+     echo 'doors array exists'; 
 
+// This is to determine if $doors has 100 items in it.    
+if (assert (count ($doors) ==100) )
+    echo ' 100 items exist in this array';
+
+
+/* 
+* This is a function that looks at every door 
+* using a loop to see if its opened or closed.
+*/
+
+function  test_doors ($doors)
+{
+    for ( $i = 1; $i <= 100; $i++)
+    {
+        assert ($doors [ $i ] == 'open');
+        echo $i . ' door ' . $doors [$i];
+    }/* end for loop */
+} /* end function test_doors */
+
+//test_doors ($doors);
+
+function test_doors_2 ( $doors )
+{
+for ( $i = 1; $i <= 100; $i++)
+
+{
+    if (( $i % 2 ) == 0 )
+       if ( assert ($doors [ $i ] == 'closed') )
+            echo $i . ' is closed ';
+        
+} /* end for loop */       
+} /* end function test_doors_2 */
+test_doors_2 ($doors);
