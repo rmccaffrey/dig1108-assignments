8279bce22b2b1f4ef0b863175e52dba44809d378
OpenShift guest <rmccaffrey@mail.valenciacollege.edu>
2012-11-19 18:25:55 -0500

    may have done it only with loops

--- switchCommand.php
+++ switchCommand.php
@@ -10,16 +10,19 @@ echo "Enter the Matrix Production Code";
 // Create an array and fill it.
 $doors= array ( ); 
 $doors = array_fill (1,10,'closed');
-
+/**
 print_r(array_values($doors));
 
 // All 100 doors are incrementing. If the doors are closed we're opening it.
 for ($i = 1; $i <= 10; $i++ )
-{
-    if ( $doors [ $i ] == 'closed')
-         $doors [ $i ] = 'open';
-    else
+{  
+   if (( $i % 1 ) == 0 )
+    {
+        if ( $doors [ $i ] == 'closed')
+         $doors [ $i ] = 'open'; 
+        else
         $doors [ $i ] = 'closed';
+    }  
 }
 print_r(array_values($doors));
 
@@ -34,7 +37,6 @@ for ($i = 1; $i <= 10; $i++ )
         $doors [ $i ] = 'closed';
     }  
 }
-
 print_r(array_values($doors));
 
 // switch state from open to close every THIRD doors
@@ -48,16 +50,55 @@ for ($i = 1; $i <= 10; $i++ )
         $doors [ $i ] = 'closed';
     }  
 }
-
 print_r(array_values($doors));
 
 
+function doorFour ($i, $doors)
+{
+    for ($i = 1; $i <= 10; $i++ )
+    {  
+        if (( $i % 4 ) == 0 )
+        {
+            if ( $doors [ $i ] == 'closed')
+            $doors [ $i ] = 'open'; 
+            else
+            $doors [ $i ] = 'closed';
+        } 
+    return print_r(array_values($doors));
+    }
+}
+
+doorFour ($i, $doors);
 
 
 
 
 
+* nested loop
 
+outer loop to iterate the doors
+*/
+
+for ($pass = 1; $pass <= 5; $pass++ ) // number of passes
+{
+    for ($i = 1; $i <= 10; $i++ ) // number of doors
+    {
+        if (( $i % $pass ) == 0 )
+            {
+                if ( $doors [ $i ] == 'closed')
+                $doors [ $i ] = 'open'; 
+                else
+                $doors [ $i ] = 'closed';
+            } 
+    }
+    print_r(array_values($doors));
+}
+
+
+
+
+
+//inner loop to check if door is open or closed
 
 
 
