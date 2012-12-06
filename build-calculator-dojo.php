40415bff6c1942414b03032d3cd9c8d61e5fcf31
OpenShift guest <rmccaffrey@mail.valenciacollege.edu>
2012-12-06 06:36:13 -0500

    I see a new pattern in the refactored test I need to figure out and refactor

--- build-calculator-dojo.php
+++ build-calculator-dojo.php
@@ -9,7 +9,7 @@
 
 require_once "production-calculator.php";
 
-
+//fuction to test addition
 function test ($operator, $a , $b)
 {
     if (assert ($operator ($a , $b ) == ($a + $b)))  
@@ -26,6 +26,24 @@ test ('add',-1,00);
 test ('add',-1,01);
 test ('add',-1,-1);
 
+//fuction to test subtraction
+function testSub ($operator, $a , $b)
+{
+    if (assert ($operator ($a , $b ) == ($a - $b)))  
+    echo 'Pass: ' . $operator . ' ' . $a . ' ' . $b;
+}
+
+testSub ('sub',00,00);
+testSub ('sub',00,01);
+testSub ('sub',00,-1);
+testSub ('sub',01,00);
+testSub ('sub',01,01);
+testSub ('sub',01,-1);
+testSub ('sub',-1,00);
+testSub ('sub',-1,01);
+testSub ('sub',-1,-1);
+
+//there is another pattern here I have to figure out.
 
 /*
 if (assert ("add(0,0) == 0")) echo "pass add 00 00";
--- production-calculator.php
+++ production-calculator.php
@@ -6,10 +6,20 @@
     *the first 'echo' line is to verify I'm calling the correct file
 */
 
+//text confimratino that we call this file
 echo "this is the production calculator";
 
+
+//fuction to add two value
 function add ($a, $b)
 {
     return $a + $b;    
 }
 
+
+//function to subtract two values
+
+function sub ($a, $b)
+{
+    return $a - $b;
+}
