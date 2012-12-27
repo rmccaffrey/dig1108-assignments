46ae86e643c1269a454ac5e465df3e4365338a47
OpenShift guest <rmccaffrey@mail.valenciacollege.edu>
2012-12-27 10:51:07 -0500

    tested instance of calculator, making a mistake with  not being an integer

--- ObjectCalc_test.php
+++ ObjectCalc_test.php
@@ -3,9 +3,19 @@
 //call the production code
 require_once 'ObjectCalc_prod.php';
 
+//write a generic pass/fail test
 function test ($assertion, $message = null)
 {
     echo (@assert($assertion) ? 'pass ' : 'fail ') . $message . "\n";
 }
 
+//test for the existance of a Calculator class
 test (class_exists('Calculator'), "A calculator class exists");
+
+//test for the existance of a new calcualtor
+test ($calculator != 0, "There is a new calculator");
+
+$calculator = new Calculator;
+
+//test for the existance of a new calcualtor
+test ($calculator != 0, "There is a new calculator");
