edaa16743dda5d2f1d4542b024fbbfbdc417c82b
OpenShift guest <rmccaffrey@mail.valenciacollege.edu>
2013-03-02 11:57:02 -0500

    added updated Matrix files

--- Matrix_production_code.php
+++ Matrix_production_code.php
@@ -1,34 +1,8 @@
 <?php
 
 /**
-    *this is the production code for the Matrix dojo
-    
+ * this verifys we're calling the matrix production code
 */
 
-echo "Enter the Matrix Production Code";
+echo "Enter the Matrix production code.";
 
-// Create an array and fill it.
-$doors= array ( ); 
-$doors = array_fill (1,100,'closed');
-
-print_r(array_values($doors));
-
-// All 100 doors are incrementing. If the doors are closed we're opening it.
-for ($i = 1; $i <= 100; $i++ )
-{
-    if ( $doors [ $i ] == 'closed')
-         $doors [ $i ] = 'open'; 
-}
-print_r(array_values($doors));
-
-// switch state from open to close on EVEN doors
-for ($i = 1; $i <= 100; $i++ )
-{  
-   if (( $i % 2 ) == 0 )
-    {
-        if ( $doors [ $i ] == 'open')
-         $doors [ $i ] = 'closed'; 
-    } /* end if */    
-}/* end for loop */  
-
-print_r(array_values($doors));
--- Matrix_tests.php
+++ Matrix_tests.php
@@ -1,74 +1,19 @@
 <?php
+
 /**
- * testing module for the Matrix dojo
- *
- * Specs are:
- *
- * Imagine a hallway with 50 closed doors on each side
- *
- * Go to the end of the hall, back up and open every door
- *
- * Go back and close even-numbered doors
- *
- * Go back and every third door, open if closed, close if open
- *
- * Continue the iteration 100 times.
- *
- * Tell me the state of all doors   
- */
-
-require_once "Matrix_production_code.php";
-
-// This section is to determine if an array $doors exists.
-if  ( is_array ($doors))
-     echo 'doors array exists'; 
-
-// This is to determine if $doors has 100 items in it.    
-if (assert (count ($doors) ==100) )
-    echo ' 100 items exist in this array';
-
-
-/* 
-* This is a function that looks at every door 
-* using a loop to see if its opened or closed.
+ * Imagine a hallway with 100 doors, 50 per side
+ * 
+ * Verify all of the doors are closed.
+ * 
+ * pass through the hall and open every door
+ * 
+ * pass through again and close every other door
+ * 
+ * pass through a third time and switch the state of every third door
+ * 
+ * continue this for 100 times
+ * 
+ * tell me the state of every door at the end of this process
 */
 
-function  test_doors ($doors)
-{
-    for ( $i = 1; $i <= 100; $i++)
-    {
-        assert ($doors [ $i ] == 'open');
-        echo $i . ' door ' . $doors [$i];
-    }/* end for loop */
-} /* end function test_doors */
-
-//This fuction test even doors and changes their state
-
-function test_even_doors ( $doors )
-{
-    for ( $i = 1; $i <= 100; $i++)
-{
-    if (( $i % 2 ) == 0 )
-       if ( assert ($doors [ $i ] == 'closed') )
-            echo $i . ' is closed ';
-        
-} /* end for loop */       
-} /* end function test_doors_2 */
-
-test_even_doors ($doors);
-
-
-//This fuction test every third door and change its state
-
-function test_third_door ( $doors )
-{
-    for ( $i = 1; $i <= 100; $i++)
-{
-    if (( $i % 3 ) == 0 )
-       if ( assert ($doors [ $i ] == 'closed') )
-            echo $i . ' is closed ';
-        
-} /* end for loop */       
-} /* end function test_doors_2 */
-
-test_even_doors ($doors);
+require_once "Matrix_production_code.php";
