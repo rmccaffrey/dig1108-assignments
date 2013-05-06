005285dc15f858248f8bbeb4e7e6127239ab62a5
OpenShift guest <rmccaffrey@mail.valenciacollege.edu>
2013-05-06 10:56:20 -0400

    first solo attempt at the Roman Numeral Calculator

--- RomanNumeral.php
+++ RomanNumeral.php
@@ -2,3 +2,8 @@
 
 //statement to ensure we call the production code
 echo "It's the Roman Numeral Converter, ya'll!" . "\n";
+
+class Calculator
+{
+    public $register = 0;
+}
--- RomanNumeral_Test.php
+++ RomanNumeral_Test.php
@@ -3,3 +3,46 @@
 //call the production code
 require_once 'RomanNumeral.php';
 
+/*
+ *When asserting an Arabic number, the calculator should convert it to a
+ *
+ *Roman numeral. Given a Roman numeral, it should return an Arabic number.
+ *
+ *1 = I, 2 = II, 3 = III, 4 = IV, 5 = V, 6 = VI, 7 = VII, 8 = VIII, 9 = IX,
+ *
+ *X = 10
+ *
+ *Logic is based on 5s. If x <= 5, do (I conversions), if x = 5 do (V 
+ *
+ *conversions). Try to deal up to 20 for now
+ */
+ 
+ //write generic test message
+ 
+ function test ($assertion, $message = null)
+ {
+  echo (@assert($assertion) ? 'pass ' : 'fail ') . $message . "\n";
+ }
+ 
+ //test that a calculator class exists
+test (class_exists('Calculator'), "A calculator class exists");
+
+ //create a new instance of the calculator
+$calculator = new Calculator;
+ 
+ //test for new instance of a calculator
+test (($calculator != null), "There is a new calculator");
+ 
+ //test that the new calculator is set to 0
+test ($calculator->register == 0, "The calculator is set to 0");
+
+ 
+ //test that the number 1 is returned as I
+ 
+ //test that the number 2 is returned as II
+ 
+ //test that the number 3 is returned as III
+ 
+ //test that the number 4 is returned as IV
+ 
+ //test that the number 5 is returned as V
