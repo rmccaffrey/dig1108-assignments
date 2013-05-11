fe0d3ece7918ae7748a1f906962db45f51b8fdcb
OpenShift guest <rmccaffrey@mail.valenciacollege.edu>
2013-05-10 22:45:17 -0400

    added new thoughts for logic

--- RomanNumeral_Test.php
+++ RomanNumeral_Test.php
@@ -2,3 +2,24 @@
 
 //call the production code
 require_once 'RomanNumeral.php';
+
+
+/**
+ * Roman numeral calculator should convert arabic numbers to
+ * 
+ * Roman numerals. Consider whether this means an array with 
+ * 
+ * places for Is, Vs, Xs, etc. Try to do up to 20 to simply
+ * 
+ * problem for now. Consider how every 5 numbers there is a 
+ * 
+ * 'less one' function (for 4, 9, 14, & 19). Consider that 
+ * 
+ * you only need three Roman numerals (I, V, & X) to count to
+ * 
+ * 20. However, you need up to 5 'places' to write numbers
+ * 
+ * through 20 (XVIII being the specific example).
+ */
+ 
+ 
