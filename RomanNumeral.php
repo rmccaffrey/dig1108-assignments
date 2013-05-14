fadb819f266ba7c11ba08148d3ccb47cef78acf9
OpenShift guest <rmccaffrey@mail.valenciacollege.edu>
2013-05-14 15:43:33 -0400

    green on converting 1 to I

--- RomanNumeral.php
+++ RomanNumeral.php
@@ -6,4 +6,13 @@ echo "It's the Roman Numeral Converter, ya'll!" . "\n";
 class Calculator
 {
     public $register = 0;
+    public function convert_i ($a)
+    {
+        if ($a == 1)
+        {
+            $this->register = "I";
+            
+            return $this->register;
+        }
+    }
 }
