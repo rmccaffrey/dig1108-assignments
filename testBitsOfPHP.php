275c77950db5e6d563fb895c851360803ee8e66d
Rob McCaffrey <rmccaffrey@mail.valenciacollege.edu>
2015-06-09 19:13:32 +0000

    First successful calculator running from 1 to 100.

--- testBitsOfPHP.php
+++ testBitsOfPHP.php
@@ -6,39 +6,80 @@ echo "\n";
 
 echo str_repeat("Rob is so cool." . "\n", 3) . (str_repeat("ha", 2)) . "\n";
 
-function numTest ($a, $x = null, $v = null, $i = null, $r = null)
+function numTest ($a, $ninty = null, $l = null, $forty = null, $x = null, $v = null, $i = null, $r = null)
 {
-    while ($a >= 10)
-    {
+    if ($a == 100){
+        $r = "C";
+        
+        $a = 0;
+    }
+    
+    if ($a >= 90) {
+        
+        $ninty = "CX";
+        
+        $a = ($a - 90);
+        
+    } 
+    while ($a > 50) {
+        $l = ($l + 1);
+        
+        $a = ($a - 50);
+    }    
+    
+    if ($a == 50) {
+        $r = "L";
+        
+        $a = 0;
+    }
+    
+    if ($a >= 40) {
+        
+        $forty = "XL";
+        
+        $a = ($a - 40);
+        
+    }   
+    
+    while ($a >= 10) {
         $x = ($x + 1);
         
         $a = ($a - 10);
         
-        if ($a == 9)
-        {
-            $r = "IX";
-            
-        }
+       
     }
     
-    while ($a >= 5)
-    {
+     if ($a == 9) {
+            
+            $r = "IX";
+            
+            $a = 0;
+            
+    }
+        
+    while ($a >= 5) {
         $v = ($v + 1);
         
         $a = ($a - 5);
         
-        
     }
     
-    while ($a >= 1)
-    {
+    if ($a == 4) {
+            
+            $r = "IV";
+            
+            $a = 0;
+            
+    }
+    
+    while ($a >= 1) {
         $i = ($i +1);
         
         $a = ($a - 1);
     }
     
-    echo str_repeat("X", $x) . str_repeat("V", $v) . str_repeat("I", $i) . $r;
+    echo $ninty . str_repeat("L", $l) . $forty . str_repeat("X", $x) . str_repeat("V", $v) . str_repeat("I", $i) . $r;
 }
 
-numTest (39);
+numTest (99);
 ?>
